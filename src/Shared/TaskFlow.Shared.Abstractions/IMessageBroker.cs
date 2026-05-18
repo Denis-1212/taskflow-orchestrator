1558d4a2894b433cd2c99e128bead5d9990e9164
SalnikovDV <d.v.salnikoff@gmail.com>
2026-05-18 15:06:15 +0700

    unused classes removed

--- src/Shared/TaskFlow.Shared.Abstractions/IMessageBroker.cs
+++
@@ -1,6 +0,0 @@
-namespace TaskFlow.Shared.Abstractions;
-
-public interface IMessageBroker
-{
-    Task PublishAsync<T>(T message, CancellationToken cancellationToken = default) where T : IEvent;
-}
